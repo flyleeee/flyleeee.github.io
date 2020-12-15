d941f4f619de5e227ee09d99781f228b6f544974
flyleeee <wyb896409234@163.com>
2020-12-15 13:25:28 +0800

    Update Hexo Static Content

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?45a2717f34b5bacc9c9f2561ed7616f7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
